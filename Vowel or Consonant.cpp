150caba4e084af584189c7c0b7d8ff1d56df6fc3
Shormi Rani Nath <shormirnath@gmail.com>
2024-12-08 20:05:24 +0600

    Create Vowel or Consonant.cpp

---
+++ Vowel or Consonant.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+
+
+    char ch;
+    scanf("%c",&ch);
+
+    switch(ch)
+    {
+     case 'a':
+     printf("vowel\n");
+
+         break;
+
+     case 'e':
+         printf("vowel\n");
+         break;
+     case 'i':
+         printf("vowel\n");
+         break;
+      case 'o':
+         printf("vowel\n");
+         break;
+       case 'u':
+         printf("vowel\n");
+         break;
+
+
+
+     default:
+      printf("consonant\n");
+      break;
+
+    }
+
+    return 0;
+
+
+
+}
